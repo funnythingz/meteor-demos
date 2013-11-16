eec3f57a9c1506540408090531471d7a9120bf8e
funnythingz <hiroki@hackleberry.me>
2013-11-16 12:43:57 +0900

    Fixed

---
+++ iron-router-demo/client/template.js
@@ -0,0 +1,23 @@
+Template.layoutTpl.helpers({
+
+  homeNav: function () {
+    return "home";
+  },
+
+  aboutNav: function () {
+    return "about";
+  },
+
+  contactNav: function () {
+    return "contact";
+  }
+  
+});
+
+Template.footerTpl.helpers({
+
+  copyright: function () {
+    return "funnythingz";
+  }
+  
+});
