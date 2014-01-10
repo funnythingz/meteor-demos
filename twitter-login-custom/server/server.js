4610d179c642f1404dbe8bc2d2cfd65939ec9a24
funnythingz <hiroki@hackleberry.me>
2014-01-10 14:29:25 +0900

    added twitter-login-custom

---
+++ twitter-login-custom/server/server.js
@@ -0,0 +1,9 @@
+Accounts.loginServiceConfiguration.remove({
+  service: 'twitter'
+});
+
+Accounts.loginServiceConfiguration.insert({
+  service: 'twitter',
+  consumerKey: '6MG4CIjMEDpw858T5jSzsQ',
+  secret: 'hTihrX2EEKjVWzfyU1RnKEK5GYtD6pLA4OMvDLyc0Q'
+});
