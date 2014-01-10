04617ba4af0755d14f3abdff01ef56abd38edaea
funnythingz <hiroki@hackleberry.me>
2014-01-10 15:52:02 +0900

    remove token

--- twitter-login-custom/server/server.js
+++ twitter-login-custom/server/server.js
@@ -4,6 +4,6 @@ Accounts.loginServiceConfiguration.remove({
 
 Accounts.loginServiceConfiguration.insert({
   service: 'twitter',
-  consumerKey: '6MG4CIjMEDpw858T5jSzsQ',
-  secret: 'hTihrX2EEKjVWzfyU1RnKEK5GYtD6pLA4OMvDLyc0Q'
+  consumerKey: '',
+  secret: ''
 });
